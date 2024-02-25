19fdddd8812351dffabb0286d1cbfab9e36ade59
erhan ozturk <eozturk78@gmail.com>
2024-02-25 19:11:52 +0300

    flutter toast updated

--- lib/shared/toast.dart
+++ lib/shared/toast.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:patient_app/model/navigator.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-final navigatorKey = GlobalKey<NavigatorState>();
 showToast(String msg) {
-  ScaffoldMessenger.of(NavigationService.navigatorKey.currentContext!)
-      .showSnackBar(SnackBar(
-    content: Text(msg),
-  ));
+  return Fluttertoast.showToast(
+      msg: msg,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 3,
+      backgroundColor: Colors.redAccent,
+      textColor: Colors.white,
+      fontSize: 16.0);
 }
