a6bce9b1a2837d9dff3f9788a5657d420f99c10e
eozturk78 <eozturk78@gmail.com>
2024-02-25 18:53:40 +0300

    changed toast message

--- lib/shared/toast.dart
+++ lib/shared/toast.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:patient_app/model/navigator.dart';
 
+final navigatorKey = GlobalKey<NavigatorState>();
 showToast(String msg) {
-  return Fluttertoast.showToast(
-      msg: msg,
-      toastLength: Toast.LENGTH_LONG,
-      gravity: ToastGravity.BOTTOM,
-      timeInSecForIosWeb: 3,
-      backgroundColor: Colors.redAccent,
-      textColor: Colors.white,
-      fontSize: 16.0);
+  ScaffoldMessenger.of(NavigationService.navigatorKey.currentContext!)
+      .showSnackBar(SnackBar(
+    content: Text(msg),
+  ));
 }
